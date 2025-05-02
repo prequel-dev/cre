6e83ce7f8377e450846799d996b062ce5e4ee132
Tony Meehan <tonymeehan@users.noreply.github.com>
2025-05-01 23:08:27 -0500

    Add tests and new rules

--- tests/cre_test.go
+++ tests/cre_test.go
@@ -1,25 +1,32 @@
 package tests
 
 import (
+	"context"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/prequel-dev/cre/pkg/logs"
+	"github.com/prequel-dev/cre/pkg/ruler"
+	"github.com/prequel-dev/preq/pkg/eval"
+	"github.com/prequel-dev/prequel-compiler/pkg/parser"
 	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
 )
 
 const (
-	creFolders = "cre-*"
-	testFile   = "test.log"
+	creFolders    = "cre-*"
+	creRules      = "*.yaml"
+	testLogFile   = "test.log"
+	testFPLogFile = "test-fp.log"
 )
 
 var (
-	rulesPath    = os.Getenv("RULES_PATH")
-	defaultPath  = "../rules"
-	level        = os.Getenv("LOG_LEVEL")
-	defaultLevel = "info"
+	rulesPath        = os.Getenv("RULES_PATH")
+	level            = os.Getenv("LOG_LEVEL")
+	defaultRulesPath = "../rules"
+	defaultLogLevel  = "info"
 )
 
 func initLogger() {
@@ -30,11 +37,11 @@ func TestMain(m *testing.M) {
 	initLogger()
 
 	if rulesPath == "" {
-		rulesPath = defaultPath
+		rulesPath = defaultRulesPath
 	}
 
 	if level == "" {
-		level = defaultLevel
+		level = defaultLogLevel
 	}
 
 	log.Info().Str("rulesPath", rulesPath).Msg("Starting tests")
@@ -42,7 +49,9 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func TestJson(t *testing.T) {
+func TestCres(t *testing.T) {
+
+	ctx := context.Background()
 
 	// Find all CRE directories
 	cres, err := filepath.Glob(filepath.Join(rulesPath, creFolders))
@@ -53,16 +62,76 @@ func TestJson(t *testing.T) {
 	// Read each CRE directory and run the tests
 	for _, cre := range cres {
 
+		var (
+			rulesT     *parser.RulesT
+			ruleData   []byte
+			testData   []byte
+			testFpData []byte
+			err        error
+		)
+
 		log.Info().Str("cre", cre).Msg("Reading CRE directory")
 
-		// Read the test file
-		_, err := os.ReadFile(filepath.Join(cre, testFile))
+		rules, err := filepath.Glob(filepath.Join(cre, creRules))
 		if err != nil {
-			t.Fatalf("Error reading CRE test file %s: %v", testFile, err)
+			t.Fatalf("Error finding CRE rule files: %v", err)
 		}
 
-		t.Run(filepath.Base(testFile), func(t *testing.T) {
-			log.Info().Str("cre", cre).Msg("Running test")
+		if len(rules) != 1 {
+			t.Fatalf("Expected 1 rule file, got %d", len(rules))
+		}
+
+		ruleData, err = os.ReadFile(rules[0])
+		if err != nil {
+			t.Fatalf("Error reading CRE rule file %s: %v", rules[0], err)
+		}
+
+		if rulesT, err = parser.Unmarshal(ruleData); err != nil {
+			t.Fatalf("Error unmarshalling rule file %s: %v", rules[0], err)
+		}
+
+		if len(rulesT.Rules) != 1 {
+			t.Fatalf("Expected 1 rule, got %d", len(rulesT.Rules))
+		}
+
+		var r = rulesT.Rules[0]
+		rulesT.Rules[0].Metadata.Hash, err = ruler.HashRule(r)
+		if err != nil {
+			t.Fatalf("Error hashing rule %s: %v", rules[0], err)
+		}
+
+		if ruleData, err = yaml.Marshal(rulesT); err != nil {
+			t.Fatalf("Error marshalling rule file %s: %v", rules[0], err)
+		}
+
+		testData, err = os.ReadFile(filepath.Join(cre, testLogFile))
+		if err != nil {
+			t.Fatalf("Error reading CRE test file: %v", err)
+		}
+
+		// Optional FP log file
+		testFpData, _ = os.ReadFile(filepath.Join(cre, testFPLogFile))
+
+		t.Run(filepath.Base(cre), func(t *testing.T) {
+			_, stats, err := eval.Detect(ctx, "", string(testData), string(ruleData))
+			if err != nil {
+				t.Fatalf("Error running detection: %v", err)
+			}
+
+			if stats["problems"] != uint32(1) {
+				t.Fatalf("Expected problems, got %d", stats["problems"])
+			}
+
+			if len(testFpData) > 0 {
+				_, stats, err := eval.Detect(ctx, "", string(testFpData), string(ruleData))
+				if err != nil {
+					t.Fatalf("Error running detection: %v", err)
+				}
+
+				if stats["problems"] != uint32(0) {
+					t.Fatalf("Expected no problems, got %d", stats["problems"])
+				}
+			}
 		})
 	}
 }
